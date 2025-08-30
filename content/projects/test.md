73c351a143528d58f8b6f775c3244c915bb61e5d
Loïc Baccigalupi <loic.baccigalupi@gmail.com>
2025-08-30 21:22:03 +0200

    Started transition

---
+++ content/projects/test.md
@@ -0,0 +1,7 @@
++++
+date = '2025-08-30T19:57:18+02:00'
+draft = true
+title = 'Test'
++++
+
+This is a test project.
--- sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity';
-import { projectType } from '@/sanity/schemaTypes/projectType';
-import { linkType } from '@/sanity/schemaTypes/linkType';
-import { indexInfoType } from '@/sanity/schemaTypes/indexInfoType';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [projectType, linkType, indexInfoType],
-};
